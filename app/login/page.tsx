f2c4a1e3e9d235fc6b7477daf60426686479c705
loihnt05 <23520869@gm.uit.edu.vn>
2025-09-06 13:53:50 +0700

    login form

---
+++ app/login/page.tsx
@@ -0,0 +1,282 @@
+"use client";
+import {
+  AlipayOutlined,
+  FacebookOutlined,
+  GoogleOutlined,
+  LockOutlined,
+  MobileOutlined,
+  TaobaoOutlined,
+  UserOutlined,
+  WeiboOutlined,
+  YahooOutlined,
+} from "@ant-design/icons";
+import {
+  LoginFormPage,
+  ProConfigProvider,
+  ProFormCaptcha,
+  ProFormCheckbox,
+  ProFormText,
+} from "@ant-design/pro-components";
+import { Button, Divider, Space, Tabs, message, theme } from "antd";
+import type { CSSProperties } from "react";
+import { useState } from "react";
+
+type LoginType = "phone" | "account";
+
+const iconStyles: CSSProperties = {
+  color: "rgba(0, 0, 0, 0.2)",
+  fontSize: "18px",
+  verticalAlign: "middle",
+  cursor: "pointer",
+};
+
+const Page = () => {
+  const [loginType, setLoginType] = useState<LoginType>("phone");
+  const { token } = theme.useToken();
+  return (
+    <div
+      style={{
+        backgroundColor: "white",
+        height: "100vh",
+      }}
+    >
+      <LoginFormPage
+        submitter={{
+          searchConfig: {
+            submitText: "Login",
+          },
+        }}
+        backgroundImageUrl="https://mdn.alipayobjects.com/huamei_gcee1x/afts/img/A*y0ZTS6WLwvgAAAAAAAAAAAAADml6AQ/fmt.webp"
+        logo="https://github.githubassets.com/favicons/favicon.png"
+        backgroundVideoUrl="https://gw.alipayobjects.com/v/huamei_gcee1x/afts/video/jXRBRK_VAwoAAAAAAAAAAAAAK4eUAQBr"
+        title="Github"
+        containerStyle={{
+          backgroundColor: "rgba(0, 0, 0,0.65)",
+          backdropFilter: "blur(4px)",
+        }}
+        subTitle="The world's largest code hosting platform"
+        activityConfig={{
+          style: {
+            boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.2)",
+            color: token.colorTextHeading,
+            borderRadius: 8,
+            backgroundColor: "rgba(255,255,255,0.25)",
+            backdropFilter: "blur(4px)",
+          },
+          title: "Event Title, with configurable image",
+          subTitle: "Event introduction and description",
+          action: (
+            <Button
+              size="large"
+              style={{
+                borderRadius: 20,
+                background: token.colorBgElevated,
+                color: token.colorPrimary,
+                width: 120,
+              }}
+            >
+              Take a look
+            </Button>
+          ),
+        }}
+        actions={
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <Divider plain>
+              <span
+                style={{
+                  color: token.colorTextPlaceholder,
+                  fontWeight: "normal",
+                  fontSize: 14,
+                }}
+              >
+                Other login methods
+              </span>
+            </Divider>
+            <Space align="center" size={24}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  height: 40,
+                  width: 40,
+                  border: "1px solid " + token.colorPrimaryBorder,
+                  borderRadius: "50%",
+                }}
+              >
+                <GoogleOutlined style={{ ...iconStyles, color: "#1677FF" }} />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  height: 40,
+                  width: 40,
+                  border: "1px solid " + token.colorPrimaryBorder,
+                  borderRadius: "50%",
+                }}
+              >
+                <YahooOutlined style={{ ...iconStyles, color: "#FF6A10" }} />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  height: 40,
+                  width: 40,
+                  border: "1px solid " + token.colorPrimaryBorder,
+                  borderRadius: "50%",
+                }}
+              >
+                <FacebookOutlined style={{ ...iconStyles, color: "#1890ff" }} />
+              </div>
+            </Space>
+          </div>
+        }
+      >
+        <Tabs
+          centered
+          activeKey={loginType}
+          onChange={(activeKey) => setLoginType(activeKey as LoginType)}
+          items={[
+            { key: "account", label: "Account Login" },
+            { key: "phone", label: "Phone Login" },
+          ]}
+        />
+        {loginType === "account" && (
+          <>
+            <ProFormText
+              name="username"
+              fieldProps={{
+                size: "large",
+                prefix: (
+                  <UserOutlined
+                    style={{
+                      color: token.colorText,
+                    }}
+                    className={"prefixIcon"}
+                  />
+                ),
+              }}
+              placeholder={"Username: admin or user"}
+              rules={[{ required: true, message: "Please enter username!" }]}
+            />
+            <ProFormText.Password
+              name="password"
+              fieldProps={{
+                size: "large",
+                prefix: (
+                  <LockOutlined
+                    style={{
+                      color: token.colorText,
+                    }}
+                    className={"prefixIcon"}
+                  />
+                ),
+              }}
+              placeholder={"Password: ant.design"}
+              rules={[{ required: true, message: "Please enter password!" }]}
+            />
+          </>
+        )}
+        {loginType === "phone" && (
+          <>
+            <ProFormText
+              fieldProps={{
+                size: "large",
+                prefix: (
+                  <MobileOutlined
+                    style={{
+                      color: token.colorText,
+                    }}
+                    className={"prefixIcon"}
+                  />
+                ),
+              }}
+              name="mobile"
+              placeholder={"Mobile number"}
+              rules={[
+                { required: true, message: "Please enter mobile number!" },
+                {
+                  pattern: /^1\d{10}$/,
+                  message: "Invalid mobile number format!",
+                },
+              ]}
+            />
+            <ProFormCaptcha
+              fieldProps={{
+                size: "large",
+                prefix: (
+                  <LockOutlined
+                    style={{
+                      color: token.colorText,
+                    }}
+                    className={"prefixIcon"}
+                  />
+                ),
+              }}
+              captchaProps={{
+                size: "large",
+              }}
+              placeholder={"Please enter verification code"}
+              captchaTextRender={(timing, count) => {
+                if (timing) {
+                  return `${count} Get verification code`;
+                }
+                return "Get verification code";
+              }}
+              name="captcha"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter the verification code!",
+                },
+              ]}
+              onGetCaptcha={async () => {
+                message.success(
+                  "Verification code obtained successfully! Code: 1234"
+                );
+              }}
+            />
+          </>
+        )}
+        <div
+          style={{
+            marginBlockEnd: 24,
+          }}
+        >
+          <ProFormCheckbox noStyle name="autoLogin">
+            Auto login
+          </ProFormCheckbox>
+          <a
+            style={{
+              float: "right",
+            }}
+          >
+            Forgot password
+          </a>
+        </div>
+      </LoginFormPage>
+    </div>
+  );
+};
+
+export default () => {
+  return (
+    <ProConfigProvider dark>
+      <Page />
+    </ProConfigProvider>
+  );
+};
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,56 +1,105 @@
-import React from 'react'
-import {
-  HomeFilled,
-  InfoCircleOutlined,
-  NotificationOutlined,
-  TeamOutlined,
-  AppstoreOutlined,
-  FolderOpenOutlined,
-  LockOutlined,
-  PhoneOutlined,
-} from "@ant-design/icons";
-import { Menu, MenuProps } from 'antd';
-import "../app/dashboard/custom-menu-gap.css";
+import React from "react";
 
-const menuItems = [
-  { key: "1", icon: <HomeFilled className='font-bold text-lg' />, label: "Dashboard" },
-  { key: "2", icon: <InfoCircleOutlined className='font-bold text-lg' />, label: "About" },
-  { key: "3", icon: <NotificationOutlined className='font-bold text-lg' />, label: "News & Events" },
-  { key: "4", icon: <TeamOutlined className='font-bold text-lg' />, label: "HR" },
-  { key: "5", icon: <AppstoreOutlined className='font-bold text-lg' />, label: "Services" },
-  { key: "6", icon: <FolderOpenOutlined className='font-bold text-lg' />, label: "Resources" },
-  { key: "7", icon: <LockOutlined className='font-bold text-lg' />, label: "Internal" },
-  { key: "8", icon: <PhoneOutlined className='font-bold text-lg' />, label: "Contact" },
-];
-const items: MenuProps["items"] = menuItems.map((item) => ({
-  key: item.key,
-  label: <span style={{ userSelect: "none" }} key={item.key}>{`${item.label}`}</span>,
-  icon: item.icon,
-}));
+import { Menu } from "antd";
+import MenuItem from "antd/es/menu/MenuItem";
+import LoginBtn from "./button-login";
 
 export default function Sidebar() {
   return (
-    <Menu
+    <div style={{ display: "flex", alignItems: "center", width: "100%", justifyContent: "center" }}>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <Menu
           theme="dark"
           mode="horizontal"
           defaultSelectedKeys={["1"]}
-          items={items}
+          // items={items}
           style={{
-            width: "auto",
-            flex: "none",
+            width: "full",
+            flex: "1",
             whiteSpace: "nowrap",
             display: "flex",
             minWidth: 0,
             height: "100%",
-            fontSize: "18px",
-            justifyContent: "space-around",
+            fontSize: "16px",
             alignItems: "center",
             fontWeight: "bold",
             textTransform: "uppercase",
             letterSpacing: "1px",
             fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
           }}
-          className="custom-menu-gap"
-        />
-  )
+        >
+          <MenuItem key={"1"}>
+            <span style={{ userSelect: "none" }}>Dashboard</span>
+          </MenuItem>
+          <MenuItem key={"2"}>
+            <span style={{ userSelect: "none" }}>About</span>
+          </MenuItem>
+          <Menu.SubMenu title="News & Events">
+            <MenuItem key={"3-1"}>
+              <span style={{ userSelect: "none" }}>News</span>
+            </MenuItem>
+            <MenuItem key={"3-2"}>
+              <span style={{ userSelect: "none" }}>Announcements</span>
+            </MenuItem>
+            <MenuItem key={"3-3"}>
+              <span style={{ userSelect: "none" }}>Events</span>
+            </MenuItem>
+          </Menu.SubMenu>
+          <Menu.SubMenu title="HR">
+            <MenuItem key={"4-1"}>
+              <span style={{ userSelect: "none" }}>Admissions</span>
+            </MenuItem>
+            <MenuItem key={"4-2"}>
+              <span style={{ userSelect: "none" }}>Schedules</span>
+            </MenuItem>
+            <MenuItem key={"4-3"}>
+              <span style={{ userSelect: "none" }}>Careers</span>
+            </MenuItem>
+            <MenuItem key={"4-4"}>
+              <span style={{ userSelect: "none" }}>Policies</span>
+            </MenuItem>
+            <MenuItem key={"4-5"}>
+              <span style={{ userSelect: "none" }}>Training</span>
+            </MenuItem>
+          </Menu.SubMenu>
+          <Menu.SubMenu title="Services">
+            <MenuItem key={"5-1"}>
+              <span style={{ userSelect: "none" }}>Online registration</span>
+            </MenuItem>
+            <MenuItem key={"5-2"}>
+              <span style={{ userSelect: "none" }}>Payments</span>
+            </MenuItem>
+            <MenuItem key={"5-3"}>
+              <span style={{ userSelect: "none" }}>Support</span>
+            </MenuItem>
+          </Menu.SubMenu>
+          <Menu.SubMenu title="Resources">
+            <MenuItem key={"6-1"}>
+              <span style={{ userSelect: "none" }}>Documents</span>
+            </MenuItem>
+            <MenuItem key={"6-2"}>
+              <span style={{ userSelect: "none" }}>Forms</span>
+            </MenuItem>
+            <MenuItem key={"6-3"}>
+              <span style={{ userSelect: "none" }}>Guides</span>
+            </MenuItem>
+          </Menu.SubMenu>
+          <Menu.SubMenu title="Contact">
+            <MenuItem key={"7-1"}>
+              <span style={{ userSelect: "none" }}>Info</span>
+            </MenuItem>
+            <MenuItem key={"7-2"}>
+              <span style={{ userSelect: "none" }}>Map</span>
+            </MenuItem>
+            <MenuItem key={"7-3"}>
+              <span style={{ userSelect: "none" }}>Feedback</span>
+            </MenuItem>
+          </Menu.SubMenu>
+        </Menu>
+      </div>
+      <div style={{ marginLeft: "auto", padding: "0 16px" }}>
+        <LoginBtn/>
+      </div>
+    </div>
+  );
 }
